7f9362deff44627669c4c336994d6f3e83706476
dieob <dbaezrenault@gmail.com>
2017-06-03 13:57:19 -0400

    Agregado W3Css, corregido navSmall

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -23,3 +23,13 @@ app.config(function($routeProvider) {
     templateUrl : "templates/promociones.html"
   });
 });
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navSmall");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
