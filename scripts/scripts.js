e7c73388296a39abe1a7fb42378f12bf760f15fe
dieob <dbaezrenault@gmail.com>
2020-01-26 16:48:55 -0300

    added search functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,12 @@
 
 var app = angular.module('myApp', ["ngRoute"]);
-app.controller('controller', function($scope, $http) {
+app.controller('controller', function($rootScope, $http) {
+  $rootScope.records = [
+    {city: 'Encarnacion', description:'san jose beach'},
+    {city: 'CDE', description:'shopping tour'}
+  ]
+
+  $rootScope.result = [];
 });
 app.config(function($routeProvider) {
   $routeProvider
@@ -18,12 +24,24 @@ app.config(function($routeProvider) {
   });
 });
 
-app.controller("resultsController", function($scope) {
-  $scope.records = [
-    {city: 'Encarnacion', description:'san jose beach'},
-    {city: 'CDE', description:'shopping tour'}
-  ]
+app.controller("resultsController", function($scope, $rootScope) {
 });
+
+app.controller("HomeController", function($scope, $rootScope, $location) {
+  console.log("homeController");
+  $rootScope.results = [];
+  $scope.onSubmitGetResults = function() {
+    console.log("entered function");
+    $rootScope.records.forEach(function (item, index) {
+      if(item.city === $scope.searchParameter){
+        $rootScope.results.push(item)
+        console.log("city is: "+ item.city)
+      }
+  });
+  $location.path('/destinos')
+  }
+});
+
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
